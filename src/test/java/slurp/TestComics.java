836df023f982d90cf1275156ac52212a4181d366
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-07-21 18:27:32 +0200

    disable big tests to run on default

--- src/test/java/slurp/TestComics.java
+++ src/test/java/slurp/TestComics.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
@@ -53,6 +54,7 @@ public class TestComics {
     }
 
     @Test
+    @Disabled
     public void getASingleComicAsPDF() {
         comicPage.getASingleComic(SINGLE_COMIC_URL);
     }
--- src/test/java/slurp/TestSeries.java
+++ src/test/java/slurp/TestSeries.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
@@ -58,6 +59,7 @@ public class TestSeries {
     }
 
     @Test
+    @Disabled
     public void getAllComicsFromASeriesAsPDFs() {
         seriesPage.getAllComicsFromASeries();
     }
